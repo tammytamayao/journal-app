91feb78e3584dc381987534d3d02b4c3a9d86408
tammytamayao <tammytamayao@gmail.com>
2022-10-31 13:45:30 +0800

    added-model-test-code

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,11 +1,5 @@
 require "test_helper"
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
-  include Devise::Test::IntegrationHelpers
-  
-  def setup do
-    sign_in users(:one)
-    @category = categories(:one)
-  end
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  test "USER SIGNUP/LOGIN INFO (NOT NULL)" do
+    user = users(:empty)
+
+    assert user.save, "Error: EMAIL OR PASSWORD IS BLANK"
+  end
+
 end
