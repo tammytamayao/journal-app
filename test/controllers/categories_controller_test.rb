5a5d5c410ac926e2e585827e57f13b96b06aa5e0
tammytamayao <tammytamayao@gmail.com>
2022-10-31 11:40:23 +0800

    modified-task-system-test-code

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -2,9 +2,9 @@ require "test_helper"
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
-
-  def setup
+  
+  setup do
     sign_in users(:one)
+    @category = categories(:one)
   end
-
 end
--- test/system/tasks_test.rb
+++ test/system/tasks_test.rb
@@ -1,44 +1,55 @@
 require "application_system_test_case"
 
 class TasksTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit tasks_url
+  #
+  #   assert_selector "h1", text: "Tasks"
+  # end
 
-    include Devise::Test::IntegrationHelpers
+  include Devise::Test::IntegrationHelpers
 
-    def setup
-        sign_in users(:one)
-        @category = categories(:one)
-        @task = tasks(:one)
-    end
+  def setup
+      sign_in users(:one)
+      @category = categories(:one)
+      @task = tasks(:one)
+  end
 
-    test "Show Task" do
-        visit category_task_path(@category,@task)
-        assert_selector "span", text: @task.title
-    end
+  test "Show Task" do
+      visit tasks_url(@task)
+      assert_selector "span", text: "ALL TASKS"
+  end
 
-    test "Create Task" do
-        visit new_category_task_path(@category)
-        fill_in "task[title]", with: @task.title
-        fill_in "task[body]", with: @task.body
-        fill_in "task[deadline]", with: @task.deadline
-        click_on "Submit"
-        assert_text "Task was successfully added."
-    end
+  test "Create Task" do
+      visit categories_url(@category)
+      click_on @category.title, match: :first
+      click_on "ADD"
+      fill_in "task[title]", with: @task.title
+      fill_in "task[body]", with: @task.body
+      fill_in "task[deadline]", with: @task.deadline
+      click_on "Submit"
+      assert_text "Task was successfully added."
+  end
 
-    test "Update Task" do
-        visit edit_category_task_path(@task,@category)
-        fill_in "task[title]", with: @task.title
-        fill_in "task[body]", with: @task.body
-        fill_in "task[deadline]", with: @task.deadline
-        click_on "Submit"
-        #assert_text "Task was successfully updated."
-    end
+  test "Update Task" do
+      visit categories_url(@category)
+      click_on @category.title, match: :first
+      click_on @task.title, match: :first
+      click_on "EDIT"
+      fill_in "task[title]", with: @task.title
+      fill_in "task[body]", with: @task.body
+      fill_in "task[deadline]", with: @task.deadline
+      click_on "Submit"
+      #assert_text "Task was successfully updated."
+  end
 
-    test "Delete Task" do
-        visit category_task_path(@task,@category)
-        page.accept_confirm do
-            click_on "DELETE", match: :first
-        end
-        assert_text "Task was successfully deleted."
-    end
-
+  test "Delete Task" do
+      visit categories_url(@category)
+      click_on @category.title, match: :first
+      click_on @task.title, match: :first
+      page.accept_confirm do
+          click_on "DELETE"
+      end
+      assert_text "Task was successfully deleted."
+  end
 end
